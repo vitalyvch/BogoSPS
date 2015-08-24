2cb793804a558d7b63f10466310b962bb12d7c40
Vitaly Chernooky <vitaly.chernooky@globallogic.com>
2015-08-24 10:26:08 +0300

    result processing is refined
    Signed-off-by: Vitaly Chernooky <vitaly.chernooky@globallogic.com>

--- bogosps.c
+++ bogosps.c
@@ -9,6 +9,9 @@
 enum { arr_size=0x1f };
 static long arr[arr_size+1];
 
+static double bogo_sps [2][arr_size+1] = {};
+static double bogo_time[2][arr_size+1] = {};
+
 static inline long sum(unsigned qty, unsigned chet_nechet)
 {
 	unsigned i;
@@ -35,8 +38,6 @@ static inline long delay(long long loops, unsigned qty, unsigned chet_nechet)
   return s;
 }
 
-double bogo_sps[2] = {};
-double bogo_time[2] = {};
 
 int
 calibrating_delay_loop(unsigned qty, unsigned chet_nechet)
@@ -61,10 +62,8 @@ calibrating_delay_loop(unsigned qty, unsigned chet_nechet)
 	     );
       fflush(stdout);
 
-	  if (4 == qty) {
-		  bogo_sps[chet_nechet] = loops_per_sec;
-		  bogo_time[chet_nechet] = 1.0 / bogo_sps[chet_nechet];
-	  }
+	  bogo_sps [chet_nechet][qty-1] = loops_per_sec;
+	  bogo_time[chet_nechet][qty-1] = 1.0 / bogo_sps[chet_nechet][qty-1];
       return 0;
     }
   }
@@ -94,7 +93,7 @@ main(void)
 {
 	int res = 0;
 	unsigned i;
-	double latency, performance;
+	double prev_thput = 0;
 
 	srandom(time(NULL));
 
@@ -104,11 +103,21 @@ main(void)
 	res |= calibrating_delay_loop_arr(0);
 	res |= calibrating_delay_loop_arr(1);
 
-	latency = bogo_time[1] - bogo_time[0];
-	performance = bogo_sps[0] / latency;
+	printf(" *** SUMMARY\n");
 
-	printf ("thput=%g BogoSps, latency=%g sec, perf=%g BogoSps^2\n",
-	        bogo_sps[0], latency, performance);
+	for (i=1; i<=(arr_size+1); i*=2) {
+		double latency, performance;
+
+		latency     = bogo_time[1][i-1] - bogo_time[0][i-1];
+		performance = bogo_sps [0][i-1] / latency;
+
+		if (prev_thput > bogo_sps[0][i-1])
+			break;
+
+		printf ("[%02u] thput=%g BogoSps, latency=%g sec, perf=%g BogoSps^2\n",
+				i, bogo_sps[0][i-1], latency, performance);
+		prev_thput = bogo_sps[0][i-1];
+	}
 	fflush(stdout);
 
 	return res;
